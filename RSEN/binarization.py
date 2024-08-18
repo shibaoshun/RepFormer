b5275d2d90b844bef6b79f730a2da3395f0e9235
shibaoshun <shibaoshun@users.noreply.github.com>
2024-08-18 18:09:49 +0800

    Update binarization.py

--- RSEN/binarization.py
+++ RSEN/binarization.py
@@ -6,10 +6,6 @@ def apply_threshold(x):
 
     tau = torch.mean(x)
     y = torch.where(x > tau, torch.ones_like(x), torch.zeros_like(x))
-
-    # 应用阈值处理，将所有大于0的像素设为1
-    # 其余像素设为0
-
     return y
 
 class Binarization(nn.Module):
